53581ecce451673cfb723845765e9699da7d62c1
CHARLIE <Janamytsko@gmail.com>
2025-09-30 11:38:52 +0100

    Starter a function

---
+++ ex1.py
@@ -0,0 +1,5 @@
+# make an algorithm that will return n number of natural numbers (integers)
+def natural_numbers(x):
+    # needs to accept the number from the user and create a sequense of integers
+    user_input = input(
+        'Please, enter the last number of the sequense you want to create: ')
