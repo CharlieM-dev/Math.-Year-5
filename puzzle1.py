cc7a17011681e1cd1cab208c2665b47c857510af
CHARLIE <Janamytsko@gmail.com>
2025-10-07 23:53:00 +0100

    Their solution

--- puzzle1.py
+++ puzzle1.py
@@ -42,3 +42,15 @@ def neg(n):
     if n < 0:
         arr = list(range(n, 1))
         print(*arr)
+# Their solutiondef pos(n):
+    n -= 1
+    while n >= 0:
+        print(n, end=' ')
+        n -= 1
+
+
+def neg(n):
+
+    while n <= 0:
+        print(n, end=' ')
+        n += 1
