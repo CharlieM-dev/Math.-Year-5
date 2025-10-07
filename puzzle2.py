05e95860e8390d5a3ccc7bfabc9f617da9f0c207
CHARLIE <Janamytsko@gmail.com>
2025-10-08 00:05:51 +0100

    Another task with function

---
+++ puzzle2.py
@@ -0,0 +1,12 @@
+# Given name of a person, the task is to welcome the person by printing the name with "Welcome".
+#  If name is "John", you should print "Welcome John".
+def welcomeAboard(name):
+    print(f'Welcome {name}')  # Your code here
+
+
+def WelcomeMessage():
+    name_of_user = input('Enter your name, please: ')
+    print(f'Welcome {name_of_user}!')
+
+
+WelcomeMessage()
